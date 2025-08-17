1d4e28aba2ddeb6c2b5c06dc4a00b097093ae8a5
Priyanka <padhypriyanka69@gmail.com>
2025-08-17 23:28:41 +0530

    prepare for deploy

--- gaana/src/App.jsx
+++ gaana/src/App.jsx
@@ -1,6 +1,6 @@
 import React, {useContext} from 'react'
 import Sidebar from './components/Sidebar';
-import Player from './components/player';
+import Player from './components/Player';
 import Display from './components/Display';
 import { PlayerContext } from './context/PlayerContext'
 
